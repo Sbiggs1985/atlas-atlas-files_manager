5cd78f244bbb922c196f1f974694d871b9f41888
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-11-04 18:19:22 -0600

    update

--- utils/controllers/AppController.js
+++ utils/controllers/AppController.js
@@ -1,1 +1,25 @@
+// controllers/AppController.js
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
 
+class AppController {
+  // GET /status endpoint
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).json(status);
+  }
+
+  // GET /stats endpoint
+  static async getStats(req, res) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).json(stats);
+  }
+}
+
+export default AppController;
