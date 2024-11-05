2cd8d581065125fc8343977d94b304a7099bdab5
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-11-04 18:18:49 -0600

    update

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './routes/index.js';
 
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Load all routes
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;
