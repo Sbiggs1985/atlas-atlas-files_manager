24e86571ce4417fdab5ebeb5fe892503e9bac312
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-11-07 10:38:53 -0600

    update

--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,5 @@
+import promisify from utils
+
 const redis = require('redis');
 const client = redis.createClient();
 
@@ -7,6 +9,18 @@ const redisClient = {
   isAlive() {
     return client.connected;
   },
+  // Get value from key
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    return asyncGet(key);
+  },
+  // Set key with value and duration
+  async set(key, value, duration) {
+    const asyncSet = promisify(this.client.setex).bind(this.client);
+    return asyncSet(key, duration, value);
+  }
 };
+
+
 
 module.exports = redisClient;
