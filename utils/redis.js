5b13627b762cae25c98d00ca934065c0fa3fe017
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-11-07 09:40:49 -0600

    update

--- utils/redis.js
+++ utils/redis.js
@@ -1,45 +1,12 @@
-import { createClient } from 'redis';
+const redis = require('redis');
+const client = redis.createClient();
 
-class RedisClient {
-  constructor() {
-    this.client = createClient();
+client.on('error', (err) => console.error('Redis connection error:', err));
 
-    this.client.on('error', (error) => {
-      console.error('Redis client error:', error);
-    });
-  }
-
+const redisClient = {
   isAlive() {
-    return this.client.connected;
-  }
+    return client.connected;
+  },
+};
 
-  async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (error, value) => {
-        if (error) reject(error);
-        resolve(value);
-      });
-    });
-  }
-
-  async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.set(key, value, 'EX', duration, (error) => {
-        if (error) reject(error);
-        resolve();
-      });
-    });
-  }
-
-  async del(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (error) => {
-        if (error) reject(error);
-        resolve();
-      });
-    });
-  }
-}
-
-const redisClient = new RedisClient();
-export default redisClient;
+module.exports = redisClient;
